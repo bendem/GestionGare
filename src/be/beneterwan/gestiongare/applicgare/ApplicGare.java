71ddd7ff7f94e8485d9d4007484b189f25c00cce
bendem <online@bendem.be>
2014-03-21 17:09:28 +0100

    Let netbeans generate some swing stuff

---
+++ src/be/beneterwan/gestiongare/applicgare/ApplicGare.java
@@ -0,0 +1,12 @@
+package be.beneterwan.gestiongare.applicgare;
+
+/**
+ * @author bendem
+ */
+public class ApplicGare {
+
+    public static void main(String[] args) {
+        ApplicGareFrame.main(args);
+    }
+
+}
