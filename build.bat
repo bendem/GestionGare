501fe9da9df95ffc04795eff2342acae3cb7791a
bendem <online@bendem.be>
2014-03-21 18:11:57 +0100

    Move sources to src/main and add var to build script

--- build.bat
+++ build.bat
@@ -2,10 +2,16 @@
 
 pushd %cd%
 
+set netbeansClasses=/Dropbox/école/2013-2014/Java/GestionGare/lib/swing-layout/swing-layout-1.0.4.jar
+set sourceFolder=src/main
+set buildFolder=target
+set mainPackage=be.beneterwan.gestiongare
+set mainPackageDir=be/beneterwan/gestiongare
+set mainPackageDir=%mainPackage:.=/%
+
 set clean="yes"
 set jar="yes"
 set verbose="no"
-set netbeansClasses="E:\NetBeans 8.0\platform\modules\ext\swing-layout-1.0.4.jar"
 
 rem parsage des arguments
 for %%a in (%*) do (
@@ -16,7 +22,7 @@ for %%a in (%*) do (
         echo.
         echo Available parameters : -noclean, -nojar, -v, -h
         echo.
-        echo -noclean : Don't clean the target directory before building
+        echo -noclean : Don't clean the %buildFolder% directory before building
         echo -nojar   : Don't create jar files
         echo -v       : Verbose debug infos
         echo -h       : Display this help
@@ -28,7 +34,7 @@ if %clean%=="no" goto compile
 
 echo.
 echo Nettoyage...
-cd target
+cd %buildFolder%
 IF EXIST *.jar del /Q /F *.jar
 IF EXIST classes rmdir /s /q classes
 IF NOT EXIST classes\ mkdir classes
@@ -37,21 +43,21 @@ cd ..
 :compile
 echo.
 echo Compilation du package authenticate...
-set compile=-s src -d target/classes src/be/beneterwan/gestiongare/authenticate/*.java
+set compile=-s src -d %buildFolder%/classes %sourceFolder%/%mainPackageDir%/authenticate/*.java
 if %verbose%=="yes" (
     javac -verbose %compile%
 ) else (
     javac %compile%
 )
 echo Compilation du package logins...
-set compile=-classpath target/classes -s src -d target/classes src/be/beneterwan/gestiongare/logins/*.java
+set compile=-classpath %buildFolder%/classes -s %sourceFolder% -d %buildFolder%/classes %sourceFolder%/%mainPackageDir%/logins/*.java
 if %verbose%=="yes" (
     javac -verbose %compile%
 ) else (
     javac %compile%
 )
 echo Compilation du package applicgare...
-set compile=-classpath "target/classes";%netbeansClasses% -s src -d target/classes src/be/beneterwan/gestiongare/applicgare/*.java
+set compile=-classpath "%buildFolder%/classes";%netbeansClasses% -s %sourceFolder% -d %buildFolder%/classes %sourceFolder%/%mainPackageDir%/applicgare/*.java
 if %verbose%=="yes" (
     javac -verbose %compile%
 ) else (
@@ -61,22 +67,22 @@ if %verbose%=="yes" (
 if %jar%=="no" goto end
 :jar
 echo.
-cd target/classes
-set makeJar=../authenticate.jar be/beneterwan/gestiongare/authenticate/
+cd %buildFolder%/classes
+set makeJar=../authenticate.jar %mainPackageDir%/authenticate/
 echo Creation de authenticate.jar...
 if %verbose%=="yes" (
     jar cvf %makeJar%
 ) else (
     jar cf %makeJar%
 )
-set makeJar=../gestiongare.jar ../../manifest.mf be.beneterwan.gestiongare.logins.FenLogin be/beneterwan/gestiongare/logins/
+set makeJar=../gestiongare.jar ../../manifest.mf %mainPackage%.logins.FenLogin %mainPackageDir%/logins/
 echo Creation de gestiongare.jar...
 if %verbose%=="yes" (
     jar cvfme %makeJar%
 ) else (
     jar cfme %makeJar%
 )
-set makeJar=../Applic_Gare.jar ../../manifest.mf be.beneterwan.gestiongare.applicgare.ApplicGare be/beneterwan/gestiongare/applicgare/
+set makeJar=../Applic_Gare.jar ../../manifest.mf %mainPackage%.applicgare.ApplicGare %mainPackageDir%/applicgare/
 echo Creation de Applic_Gare.jar...
 if %verbose%=="yes" (
     jar cvfme %makeJar%
