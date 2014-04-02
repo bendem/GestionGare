246c6d68c630a6dc186f1ec12b9828f76fe41d35
bendem <online@bendem.be>
2014-04-02 11:30:20 +0200

    Set window position and parent

--- src/main/java/be/beneterwan/gestiongare/applicgare/help/DateFormatFrame.java
+++ src/main/java/be/beneterwan/gestiongare/applicgare/help/DateFormatFrame.java
@@ -6,17 +6,22 @@
 
 package be.beneterwan.gestiongare.applicgare.help;
 
+import be.beneterwan.gestiongare.applicgare.ApplicGareFrame;
+
 /**
  *
  * @author Ben
  */
 public class DateFormatFrame extends javax.swing.JFrame {
 
-    /**
-     * Creates new form DateFormatFrame
-     */
-    public DateFormatFrame() {
+    private final ApplicGareFrame parent;
+
+    public DateFormatFrame(ApplicGareFrame parent) {
         initComponents();
+        this.parent = parent;
+        setLocationRelativeTo(null);
+        pack();
+        setVisible(true);
     }
 
     /**
@@ -44,46 +49,6 @@ public class DateFormatFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for(javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
-                    .getInstalledLookAndFeels()) {
-                if("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch(ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(DateFormatFrame.class.getName())
-                    .log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(InstantiationException ex) {
-            java.util.logging.Logger.getLogger(DateFormatFrame.class.getName())
-                    .log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(DateFormatFrame.class.getName())
-                    .log(java.util.logging.Level.SEVERE, null, ex);
-        } catch(javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(DateFormatFrame.class.getName())
-                    .log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new DateFormatFrame().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
