b5df267333397ccb563b73eb52306828cc71be86
bendem <online@bendem.be>
2014-04-25 17:32:32 +0200

    Handle bugged lib
    We have to use the provided lib even if it's bugged...

--- src/main/java/be/beneterwan/gestiongare/applicgare/ApplicDepotReceiver.java
+++ src/main/java/be/beneterwan/gestiongare/applicgare/ApplicDepotReceiver.java
@@ -1,12 +1,16 @@
 package be.beneterwan.gestiongare.applicgare;
 
+import be.beneterwan.gestiongare.commons.logger.CustomLogger;
 import be.beneterwan.gestiongare.commons.threads.AbstractRunnable;
+import java.util.logging.Logger;
 import network.NetworkStringReceiver;
 
 /**
  * @author bendem et Curlybear
  */
 public class ApplicDepotReceiver extends AbstractRunnable {
+
+    private static final Logger LOGGER = new CustomLogger(AbstractRunnable.class.getSimpleName());
 
     private final ApplicGare applicGare;
     private NetworkStringReceiver receiver;
@@ -30,7 +34,11 @@ public class ApplicDepotReceiver extends AbstractRunnable {
 
     @Override
     protected void shutdown() {
-        receiver.endReceiving();
+        try {
+            receiver.endReceiving();
+        } catch(NullPointerException e) {
+            LOGGER.severe("Could not stop receiving, the program will not shutdown correctly!");
+        }
     }
 
 }
