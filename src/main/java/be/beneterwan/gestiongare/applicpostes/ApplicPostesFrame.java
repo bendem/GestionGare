f12bdbf4c889bdec6b615460d4b0c0c24a093f98
bendem <online@bendem.be>
2014-05-02 16:27:04 +0200

    Random cleanup

--- src/main/java/be/beneterwan/gestiongare/applicpostes/ApplicPostesFrame.java
+++ src/main/java/be/beneterwan/gestiongare/applicpostes/ApplicPostesFrame.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package be.beneterwan.gestiongare.applicpostes;
 
 import be.beneterwan.gestiongare.commons.ResourceManager;
 import javax.swing.ImageIcon;
 
 /**
- *
- * @author Bear
+ * @author bendem et Curlybear
  */
 public class ApplicPostesFrame extends javax.swing.JFrame {
 
-    /**
-     * Creates new form ApplicPostesFrame
-     */
     public ApplicPostesFrame() {
         super("Applic Postes");
         initComponents();
@@ -161,41 +151,6 @@ public class ApplicPostesFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ApplicPostesFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ApplicPostesFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ApplicPostesFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ApplicPostesFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ApplicPostesFrame().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton buttonMsgRecu;
