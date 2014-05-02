942a6a656b28190fcba07a3e98857c3f094b46c9
bendem <online@bendem.be>
2014-05-02 14:30:18 +0200

    Add schedule generation module

---
+++ src/main/java/be/beneterwan/gestiongare/schedulegenerator/ScheduleGenerator.java
@@ -0,0 +1,17 @@
+package be.beneterwan.gestiongare.schedulegenerator;
+
+import be.beneterwan.gestiongare.commons.logger.CustomLogger;
+import java.util.logging.Logger;
+
+/**
+ * @author bendem et Curlybear
+ */
+public class ScheduleGenerator {
+
+    private static final Logger LOGGER = new CustomLogger(ScheduleGenerator.class.getSimpleName());
+
+    public static void main(String[] args) {
+        LOGGER.info("Generating schedules...");
+    }
+
+}
