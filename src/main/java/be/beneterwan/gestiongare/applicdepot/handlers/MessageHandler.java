367b2108ba57277bdab84b3933006aac119d47b7
bendem <online@bendem.be>
2014-05-10 19:23:52 +0200

    Fix compilation

--- src/main/java/be/beneterwan/gestiongare/applicdepot/handlers/MessageHandler.java
+++ src/main/java/be/beneterwan/gestiongare/applicdepot/handlers/MessageHandler.java
@@ -2,7 +2,10 @@ package be.beneterwan.gestiongare.applicdepot.handlers;
 
 import be.beneterwan.gestiongare.applicdepot.ApplicDepot;
 import be.beneterwan.gestiongare.commons.eventmanagement.EventHandler;
+import be.beneterwan.gestiongare.commons.network.messages.Message;
+import be.beneterwan.gestiongare.commons.network.messages.TrainMessage;
 import be.beneterwan.gestiongare.commons.network.receiver.MessageEvent;
+import be.beneterwan.gestiongare.commons.trains.Train;
 import java.util.EventObject;
 
 /**
@@ -19,9 +22,11 @@ public class MessageHandler implements EventHandler {
 
     @Override
     public void execute(EventObject event) {
-        MessageEvent mEvent = (MessageEvent)event;
-
-        applicDepot.getFrame().getFieldAnnonce().setText(mEvent.getMessage());
+        Message message = ((MessageEvent) event).getMessage();
+        if(!message.getType().equals(Message.Type.Ack)) {
+            Train train = ((TrainMessage) message).getTrain();
+            applicDepot.getFrame().getFieldAnnonce().setText(train.toString());
+        }
     }
 
 }
