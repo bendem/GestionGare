cb358ff779064e620438f253ff652436d1dadc07
Curlybear <tehcurlybear@gmail.com>
2014-05-13 15:15:45 +0200

    Handler ControlOut

--- src/main/java/be/beneterwan/gestiongare/applicgare/handlers/ControleOutHandler.java
+++ src/main/java/be/beneterwan/gestiongare/applicgare/handlers/ControleOutHandler.java
@@ -2,7 +2,7 @@ package be.beneterwan.gestiongare.applicgare.handlers;
 
 import be.beneterwan.gestiongare.applicgare.ApplicGare;
 import be.beneterwan.gestiongare.commons.eventmanagement.EventHandler;
-import be.beneterwan.gestiongare.commons.network.messages.TrainComing;
+import be.beneterwan.gestiongare.commons.trains.HoraireTrain;
 import java.util.EventObject;
 
 /**
@@ -18,5 +18,14 @@ public class ControleOutHandler implements EventHandler {
 
     @Override
     public void execute(EventObject event) {
+        int id = applicGare.getFrame().getTableOccupationVoies().getSelectedRow();
+        HoraireTrain train;
+        
+        if ( applicGare.getFrame().getTableOccupationVoies().getSelectedRow() != -1) {
+            train=applicGare.getTrainManager().getInboundTrains().get(id);
+            if (train.getState()== HoraireTrain.State.Stationned) {
+                applicGare.getTrainManager().trainLeaving(train);
+            }
+        }
     }
 }
