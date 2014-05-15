8dbdff4d101b4a6a031a4db017c3bcb219a02930
bendem <online@bendem.be>
2014-05-15 20:28:19 +0200

    The logger now save the previous log file...
    ... when first instanciated by the application
    + Logs are now separated by jars

--- src/main/java/be/beneterwan/gestiongare/commons/logger/CustomLogger.java
+++ src/main/java/be/beneterwan/gestiongare/commons/logger/CustomLogger.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,15 +16,23 @@ import java.util.logging.Logger;
  */
 public class CustomLogger extends Logger {
 
+    private static final String LOG_FOLDER_PATH = "." + File.separator + "logs" + File.separator;
     private static final Formatter formatter = new CustomFormatter();
-    private static final File logFile;
+    private static final Set<String> firstRun = new HashSet<>();
 
-    static {
-        logFile = createLogFile();
-    }
+    private final File logFile;
 
-    public CustomLogger(String name) {
-        super(name, null);
+    public CustomLogger(Class<?> clazz) {
+        super(clazz.getSimpleName(), null);
+
+        String jarName = getJarName(clazz);
+        logFile = new File(LOG_FOLDER_PATH + "log_" + jarName + ".txt");
+
+        // Move logs when the CustomLogger is initialized for the first time for a specific jar
+        if(!firstRun.contains(jarName)) {
+            backupLogs();
+            firstRun.add(jarName);
+        }
 
         // Set global level to all and refine it in the handlers
         setLevel(Level.ALL);
@@ -46,8 +56,12 @@ public class CustomLogger extends Logger {
         }
     }
 
-    private static File createLogFile() {
-        File file = new File("." + File.separator + "logs" + File.separator + "logs.txt");
+    private String getJarName(Class<?> clazz) {
+        return new File(clazz.getProtectionDomain().getCodeSource().getLocation().getPath()).getName();
+    }
+
+    private File createLogFile() {
+        File file = new File(LOG_FOLDER_PATH + "logs.txt");
         createLogFolder(file.getParentFile());
         if(!file.exists()) {
             try {
@@ -61,9 +75,19 @@ public class CustomLogger extends Logger {
         return file;
     }
 
-    private static void createLogFolder(File folder) {
+    private void createLogFolder(File folder) {
         if(!folder.exists()) {
             folder.mkdir();
+        }
+    }
+
+    private void backupLogs() {
+        File previousLogs = new File(LOG_FOLDER_PATH + "previous_" + logFile.getName());
+        if(previousLogs.exists()) {
+            previousLogs.delete();
+        }
+        if(logFile.exists()) {
+            logFile.renameTo(previousLogs);
         }
     }
 
