1ed51891a261000423d955ce6ed6343ea73c8c1e
bendem <online@bendem.be>
2014-05-15 19:55:10 +0200

    Log to console and to file using writers

--- src/main/java/be/beneterwan/gestiongare/commons/logger/CustomHandler.java
+++ src/main/java/be/beneterwan/gestiongare/commons/logger/CustomHandler.java
@@ -1,26 +1,55 @@
 package be.beneterwan.gestiongare.commons.logger;
 
+import java.io.IOException;
+import java.io.Writer;
+import java.util.logging.Formatter;
 import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 /**
  * @author bendem & Curlybear
  */
 public class CustomHandler extends Handler {
 
-    public CustomHandler() {
-        setFormatter(new CustomFormatter());
+    private final Writer writer;
+
+    public CustomHandler(Writer writer, Formatter formatter) {
+        setFormatter(formatter);
+        this.writer = writer;
     }
 
     @Override
-    public void close() {}
+    public void close() throws SecurityException {
+        try {
+            writer.close();
+        } catch(IOException ex) {
+            Logger.getLogger(CustomHandler.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }
 
     @Override
-    public void flush() {}
+    public void flush() {
+        try {
+            writer.flush();
+        } catch(IOException ex) {
+            Logger.getLogger(CustomHandler.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+    }
 
     @Override
     public void publish(LogRecord lr) {
-        System.out.println(getFormatter().format(lr));
+        String toPublish = getFormatter().format(lr) + '\n';
+        try {
+            writer.write(toPublish, 0, toPublish.length());
+            flush(); // TEMP, to remove when working...
+        } catch(IOException ex) {
+            Logger.getLogger(CustomHandler.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
     }
 
 }
--- src/main/java/be/beneterwan/gestiongare/commons/logger/CustomLogger.java
+++ src/main/java/be/beneterwan/gestiongare/commons/logger/CustomLogger.java
@@ -1,5 +1,11 @@
 package be.beneterwan.gestiongare.commons.logger;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.logging.Formatter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -8,10 +14,57 @@ import java.util.logging.Logger;
  */
 public class CustomLogger extends Logger {
 
+    private static final Formatter formatter = new CustomFormatter();
+    private static final File logFile;
+
+    static {
+        logFile = createLogFile();
+    }
+
     public CustomLogger(String name) {
         super(name, null);
+
+        // Set global level to all and refine it in the handlers
         setLevel(Level.ALL);
-        addHandler(new CustomHandler());
+
+        // Log to console
+        CustomHandler consoleHandler = new CustomHandler(new OutputStreamWriter(System.out), formatter);
+        consoleHandler.setLevel(Level.ALL);
+        addHandler(consoleHandler);
+
+        // Log to file
+        CustomHandler fileHandler = null;
+        try {
+            fileHandler = new CustomHandler(new BufferedWriter(new FileWriter(logFile, true)), formatter);
+        } catch(IOException ex) {
+            Logger.getLogger(CustomLogger.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        if(fileHandler != null) {
+            fileHandler.setLevel(Level.ALL);
+            addHandler(fileHandler);
+        }
+    }
+
+    private static File createLogFile() {
+        File file = new File("." + File.separator + "logs" + File.separator + "logs.txt");
+        createLogFolder(file.getParentFile());
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch(IOException ex) {
+                Logger.getLogger(CustomLogger.class.getName())
+                    .log(Level.SEVERE, "Could not create log file", ex);
+                return null;
+            }
+        }
+        return file;
+    }
+
+    private static void createLogFolder(File folder) {
+        if(!folder.exists()) {
+            folder.mkdir();
+        }
     }
 
 }
