6b337238810c9520bcb36fd340258c6fa088c805
bendem <online@bendem.be>
2014-05-17 15:47:58 +0200

    Add PropertyChangeSupport. Maybe?

--- src/main/java/be/beneterwan/gestiongare/applicgare/incidents/StateBean.java
+++ src/main/java/be/beneterwan/gestiongare/applicgare/incidents/StateBean.java
@@ -1,26 +1,37 @@
 package be.beneterwan.gestiongare.applicgare.incidents;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
 import java.util.Random;
 
 /**
  * @author bendem & Curlybear
  */
-public class StateBean implements NumberConsumer {
+public class StateBean implements NumberConsumer, Serializable {
 
+    private final PropertyChangeSupport propertySupport;
     private String info = "Nothing";
+
+    public StateBean()  {
+        propertySupport = new PropertyChangeSupport(this);
+    }
 
     @Override
     public void consumeNumber(double number) {
         int nbType = new Random().nextInt(20);
         IncidentType type = IncidentType.getFromNumber(nbType);
         if(type != null) {
-            firePropertyChangeEvent(/**/);
+            propertySupport.firePropertyChange("", "", "");
         }
     }
 
-    private void firePropertyChangeEvent() {
-        // TODO
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertySupport.removePropertyChangeListener(listener);
     }
 
 }
