151f7c2bc431e64695c692b256b0180aad9f4c65
bendem <online@bendem.be>
2014-05-17 16:26:37 +0200

    Really implement StateBean

--- src/main/java/be/beneterwan/gestiongare/applicgare/incidents/StateBean.java
+++ src/main/java/be/beneterwan/gestiongare/applicgare/incidents/StateBean.java
@@ -1,5 +1,7 @@
 package be.beneterwan.gestiongare.applicgare.incidents;
 
+import be.beneterwan.gestiongare.commons.ApplicationConfig;
+import be.beneterwan.gestiongare.commons.config.ConfigManager;
 import java.util.Random;
 
 /**
@@ -7,15 +9,49 @@ import java.util.Random;
  */
 public class StateBean extends AbstractBean implements NumberConsumer {
 
+    private final Random rnd;
+    private final ConfigManager configManager;
     private String info = "Nothing";
+
+    public StateBean() {
+        rnd = new Random();
+        configManager = new ConfigManager(ConfigManager.CONFIG_FILE_NAME, true);
+    }
 
     @Override
     public void consumeNumber(double number) {
-        int nbType = new Random().nextInt(20);
-        IncidentType type = IncidentType.getFromNumber(nbType);
-        if(type != null) {
-            propertySupport.firePropertyChange("info", "old_value", info);
+        // One incident at a time, please...
+        if(!info.equals("Nothing")) {
+            return;
         }
+
+        int nbType = rnd.nextInt(20);
+        String old = info;
+        IncidentType type = IncidentType.getFromNumber(nbType);
+
+        if(type == null) {
+            return;
+        }
+
+        switch(type) {
+            case Retard:
+                info = String.format(configManager.getString(ApplicationConfig.MessageIncidentRetard), rnd.nextInt(25)+5);
+                break;
+            case Manifestation:
+                info = configManager.getString(ApplicationConfig.MessageIncidentManifestation);
+                break;
+            case Greve:
+                info = configManager.getString(ApplicationConfig.MessageIncidentGreve);
+                break;
+        }
+
+        propertySupport.firePropertyChange("info", old, info);
+    }
+
+    public void reset() {
+        String old = info;
+        info = "Nothing";
+        propertySupport.firePropertyChange("info", old, info);
     }
 
 }
--- src/main/java/be/beneterwan/gestiongare/commons/ApplicationConfig.java
+++ src/main/java/be/beneterwan/gestiongare/commons/ApplicationConfig.java
@@ -8,16 +8,19 @@ import be.beneterwan.gestiongare.commons.config.Config;
  */
 public enum ApplicationConfig implements Config {
 
-    PortApplicInToApplicGare("port.ApplicInToApplicGare", 50_010),
-    PortApplicOutToApplicGare("port.ApplicOutToApplicGare", 50_011),
-    PortApplicDepotToApplicGare("port.ApplicDepotToApplicGare", 50_015),
-    PortApplicGareToApplicIn("port.ApplicGareToApplicIn", 50_000),
-    PortApplicGareToApplicOut("port.ApplicGareToApplicOut", 50_001),
-    PortApplicGareToApplicDepot("port.ApplicGareToApplicDepot", 50_005),
-    IpApplicIn("ip.ApplicIn", "127.0.0.1"),
-    IpApplicOut("ip.ApplicOut", "127.0.0.1"),
-    IpApplicGare("ip.ApplicGare", "127.0.0.1"),
-    IpApplicDepot("ip.ApplicDepot", "127.0.0.1"),
+    PortApplicInToApplicGare    ("port.ApplicInToApplicGare", 50_010),
+    PortApplicOutToApplicGare   ("port.ApplicOutToApplicGare", 50_011),
+    PortApplicDepotToApplicGare ("port.ApplicDepotToApplicGare", 50_015),
+    PortApplicGareToApplicIn    ("port.ApplicGareToApplicIn", 50_000),
+    PortApplicGareToApplicOut   ("port.ApplicGareToApplicOut", 50_001),
+    PortApplicGareToApplicDepot ("port.ApplicGareToApplicDepot", 50_005),
+    IpApplicIn                  ("ip.ApplicIn", "127.0.0.1"),
+    IpApplicOut                 ("ip.ApplicOut", "127.0.0.1"),
+    IpApplicGare                ("ip.ApplicGare", "127.0.0.1"),
+    IpApplicDepot               ("ip.ApplicDepot", "127.0.0.1"),
+    MessageIncidentRetard       ("incident.message.retard", "Retard"),
+    MessageIncidentManifestation("incident.message.manifestation", "Manifestation"),
+    MessageIncidentGreve        ("incident.message.greve", "Greve"),
     ;
 
     private final String node;
