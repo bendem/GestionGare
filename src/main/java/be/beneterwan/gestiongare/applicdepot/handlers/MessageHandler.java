88fd7610673c24d0014a0489a0ff0dcb2e58947e
Curlybear <tehcurlybear@gmail.com>
2014-05-19 19:54:12 +0200

    Answer Message CreateNewTrain

--- src/main/java/be/beneterwan/gestiongare/applicdepot/handlers/MessageHandler.java
+++ src/main/java/be/beneterwan/gestiongare/applicdepot/handlers/MessageHandler.java
@@ -2,11 +2,13 @@ package be.beneterwan.gestiongare.applicdepot.handlers;
 
 import be.beneterwan.gestiongare.applicdepot.ApplicDepot;
 import be.beneterwan.gestiongare.commons.eventmanagement.EventHandler;
-import be.beneterwan.gestiongare.commons.network.messages.Message;
+import be.beneterwan.gestiongare.commons.network.messages.CreatedNewTrainMessage;
 import be.beneterwan.gestiongare.commons.network.messages.HoraireTrainMessage;
+import be.beneterwan.gestiongare.commons.network.messages.Message;
 import be.beneterwan.gestiongare.commons.network.receiver.MessageEvent;
 import be.beneterwan.gestiongare.commons.trains.HoraireTrain;
 import java.util.EventObject;
+import java.util.Map;
 
 /**
  *
@@ -29,6 +31,19 @@ public class MessageHandler implements EventHandler {
             applicDepot.getFrame().setTrainAnnonce(horaire);
             applicDepot.getFrame().getButtonMsgRecu().setEnabled(true);
             applicDepot.getFrame().getFieldAnnonce().setText(horaire.toString());
+        } else if(message.getType().equals(Message.Type.CreateNewTrain)){
+            for(Map.Entry<Integer, HoraireTrain> en : applicDepot.getFrame().getStoredTrains().entrySet()) {
+                Integer object = en.getKey();
+                HoraireTrain object1 = en.getValue();
+
+                if(object1 != null) {
+                    CreatedNewTrainMessage mess = new CreatedNewTrainMessage(object1.getTrain());
+                    mess.send(applicDepot.getNetworkSender());
+                    applicDepot.getFrame().getStoredTrains().put(object, null);
+                    return;
+                }
+
+            }
         }
     }
 
